aa65afb3a7a0d5861df0b786f5dabcb3abf9c8f0
Maram Kamil <maram.kamil@gmail.com>
2019-02-14 09:26:32 +0300

    I hope i am a winner.

--- tourista/urls.py
+++ tourista/urls.py
@@ -34,8 +34,7 @@ urlpatterns = [
          views.destination_delete, name='destination-delete'),
 ]
 
-if settings.DEBUG:
-    '''Uncomment the next line to include your static files'''
-    # urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG: 
+    urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
